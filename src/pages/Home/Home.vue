4827b872da5c8034d03d481bdb766fdb3067e69a
jero <jeroflat@gmail.com>
2019-03-01 20:12:46 +0300

    feat(Layout): add margin-bottom when mobile

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -36,18 +36,28 @@
           на модные платьешки от китайских кутюрье
         </div>
       </div>
-      <img
-        class="img-fluid"
-        srcset="
-          assets/img/home/banner1-mobile.png 320w,
-          assets/img/home/banner1.png 1160w"
-        sizes="
-          (min-width: 576px) 100vw,
-          (min-width: 768px) 100vw,
-          (min-width: 992px) 100vw"
-        src="assets/img/home/banner1-mobile.png"
-        alt="banner1"
-      >
+      <picture>
+        <source
+          srcset="assets/img/home/banner1.png"
+          media="(min-width: 1200px)"
+        />
+        <source
+          srcset="assets/img/home/banner1.png"
+          media="(min-width: 992px)"
+        />
+        <source
+          srcset="assets/img/home/banner1.png"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="assets/img/home/banner1-small.png"
+          media="(min-width: 576px)"
+        />
+        <img
+          src="assets/img/home/banner1-small.png"
+          alt="banner 1"
+        />
+      </picture>
     </div>
 
     <section>
@@ -81,11 +91,28 @@
           на модные платьешки от китайских кутюрье
         </div>
       </div>
-      <img
-        class="img-fluid"
-        src="assets/img/home/banner2.png"
-        alt="banner1"
-      >
+       <picture>
+        <source
+          srcset="assets/img/home/banner2.png"
+          media="(min-width: 1200px)"
+        />
+        <source
+          srcset="assets/img/home/banner2.png"
+          media="(min-width: 992px)"
+        />
+        <source
+          srcset="assets/img/home/banner2.png"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="assets/img/home/banner2-small.png"
+          media="(min-width: 576px)"
+        />
+        <img
+          src="assets/img/home/banner2-small.png"
+          alt="banner 1"
+        />
+      </picture>
     </div>
 
     <section>
@@ -119,11 +146,28 @@
           Комбинзоны для активных мамочек!
         </div>
       </div>
-      <img
-        class="img-fluid"
-        src="assets/img/home/banner3.png"
-        alt="banner1"
-      >
+      <picture>
+        <source
+          srcset="assets/img/home/banner3.png"
+          media="(min-width: 1200px)"
+        />
+        <source
+          srcset="assets/img/home/banner3.png"
+          media="(min-width: 992px)"
+        />
+        <source
+          srcset="assets/img/home/banner3.png"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="assets/img/home/banner3-small.png"
+          media="(min-width: 576px)"
+        />
+        <img
+          src="assets/img/home/banner1-small.png"
+          alt="banner 1"
+        />
+      </picture>
     </div>
 
     <section>
@@ -157,11 +201,28 @@
           Как стать королевой панели? У нас есть ответ!
         </div>
       </div>
-      <img
-        class="img-fluid"
-        src="assets/img/home/banner4.png"
-        alt="banner1"
-      >
+      <picture>
+        <source
+          srcset="assets/img/home/banner4.png"
+          media="(min-width: 1200px)"
+        />
+        <source
+          srcset="assets/img/home/banner4.png"
+          media="(min-width: 992px)"
+        />
+        <source
+          srcset="assets/img/home/banner4.png"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="assets/img/home/banner1-small.png"
+          media="(min-width: 576px)"
+        />
+        <img
+          src="assets/img/home/banner1-small.png"
+          alt="banner 1"
+        />
+      </picture>
     </div>
   </section>
 </template>
@@ -275,20 +336,35 @@ export default {
 
     @include element(banner) {
       margin-bottom: px-to-rem(40);
+      overflow: hidden;
       position: relative;
+      border-top-right-radius: px-to-rem(4);
+      border-bottom-right-radius: px-to-rem(4);
+
+      @include media($xl) {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+
     }
 
     @include element(banner-text) {
       position: absolute;
-      top: px-to-rem(54);;
-      left: px-to-rem(74);;
+      top: px-to-rem(54);
+      left: px-to-rem(10);
       color: var(--white);
+
+      @include media($md) {
+        top: px-to-rem(54);
+        left: px-to-rem(74);
+      }
     }
 
     @include element(banner-title) {
       font-weight: 900;
       line-height: 40px;
       font-size: 26px;
+
       @include media($md) {
         line-height: 60px;
         font-size: 40px;
@@ -300,6 +376,7 @@ export default {
       line-height: 24px;
       font-size: 16px;
       max-width: px-to-rem(184);
+
       @include media($md) {
         line-height: 32px;
         font-size: 22px;
