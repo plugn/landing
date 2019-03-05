3546dca2bfa22881210959afb2c92ebd7146c134
jero <jeroflat@gmail.com>
2019-03-05 20:36:50 +0300

    feat(FooterTop): create content
    adjust styles

--- src/components/core/Footer/FooterTop.vue
+++ src/components/core/Footer/FooterTop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
+    <div class="col footer-top__column">
       <div>
         <router-link
           class="footer-top__logo"
@@ -17,29 +17,133 @@
         </p>
       </div>
     </div>
-    <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
-
+    <div class="col footer-top__column">
+      <h5 class="footer-top__title">
+        <strong>
+          Полезные ссылки
+        </strong>
+      </h5>
+      <div class="footer-top__links">
+        <div
+          v-for="link in firstLinks"
+          :key="link"
+          class="footer-top__link"
+        >
+          {{ link }}
+        </div>
+      </div>
     </div>
-    <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
-
+    <div class="col footer-top__column">
+      <h5 class="footer-top__title invisible">
+        <strong>
+          Полезные ссылки
+        </strong>
+      </h5>
+      <div class="footer-top__links">
+        <div
+          v-for="link in secondLinks"
+          :key="link"
+          class="footer-top__link"
+        >
+          {{ link }}
+        </div>
+      </div>
     </div>
-    <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
-
+    <div class="col footer-top__column">
+      <h5 class="footer-top__title">
+        <strong>
+          Контакты
+        </strong>
+      </h5>
+      <div class="footer-top__social-links">
+        <div
+          v-for="social in socialLinks"
+          :key="social.id"
+          :style="{background: social.background}"
+          class="footer-top__social"
+        >
+          <img
+            class="img-fluid"
+            :src="social.icon"
+          >
+        </div>
+      </div>
+     <span class="footer-top__email">Общие вопросы:</span> info@alabom.com 
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'FooterTop',
+  data() {
+    return {
+      firstLinks: [
+        'FAQ',
+        'Пользовательское соглашение',
+        'Акции',
+        'Доставка',
+        'Отзывы',
+      ],
+      secondLinks: [
+        'Политика конфиденциальности',
+        'Журнал',
+        'Скидки',
+        'Гарантии',
+        'ОтзСтать продавцом',
+      ],
+      socialLinks: [
+        {
+          id: 0,
+          icon: 'assets/img/brands/facebook.png',
+          background: '#414F91',
+        },
+        {
+          id: 1,
+          icon: 'assets/img/brands/vk.png',
+          background: '#455C81',
+        },
+        {
+          id: 2,
+          icon: 'assets/img/brands/youtube.png',
+          background: '#DD2B22',
+        },
+        {
+          id: 3,
+          icon: 'assets/img/brands/instagram.png',
+          background: '#8744C4',
+        },
+        {
+          id: 4,
+          icon: 'assets/img/brands/ok.png',
+          background: '#F0931D',
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
   @import '~styles/functions/px-to-rem';
   @import '~styles/mixins';
+  @import '~styles/variables';
 
   .footer-top {
+    @include element(column) {
+      padding: 0 0.71429rem;
+      width: 25%;
+    }
+
+    @include element(title) {
+      margin-bottom: 2.5rem;
+    }
+
+    @include element(link) {
+      line-height: 24px;
+      text-decoration-line: underline;
+    }
+
     @include element(logo) {
       display: inline-block;
       margin-bottom: px-to-rem(35);
@@ -49,6 +153,25 @@ export default {
     @include element(description) {
       color: var(--light-gray);
       font-size: 12px;
+    }
+
+    @include element(social-links) {
+      display: flex;
+      justify-content: flex-start;
+      margin-bottom: px-to-rem(25);
+    }
+
+    @include element(social) {
+      align-items: center;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      margin-right: px-to-rem(12);
+      @include size(px-to-rem(32));
+    }
+
+    @include element(email) {
+      font-weight: 500;
     }
   }
 </style>
