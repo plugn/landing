4d70f5882662d75b08d9646a92b81b8e4c94e5bd
jero <jeroflat@gmail.com>
2019-03-12 12:57:34 +0300

    refactor(utils/tFrom): use LANG constant

--- src/utils/tFrom.js
+++ src/utils/tFrom.js
@@ -1,15 +1,18 @@
 import has from 'lodash.has';
 import set from 'lodash.set';
 
-import getLang from './getLang';
+import { LANG } from '@/constants';
 
-const lang = getLang();
-
+/**
+ *
+ * @param {Array<String>} props - properties to translate
+ * @param {Object<T>} i18Object - object with keys with values in i18n format
+ * @return {Object<{[string]: any}>}
+ */
 export default function fFrom(props, i18Object) {
-  // let obj = {};
   props.forEach((prop) => {
     if (has(i18Object, prop)) {
-      set(i18Object, prop, i18Object[prop][lang]);
+      set(i18Object, prop, i18Object[prop][LANG]);
     }
   });
   return i18Object;
